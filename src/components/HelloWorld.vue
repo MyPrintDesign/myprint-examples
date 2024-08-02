41cc916c3cb7eb448c58d1a55c9b4ce468a00125
css <504640003@qq.com>
2024-08-02 14:45:11 +0800

    feat: 优化模版数据

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,8 @@
 <script setup lang="ts">
-import {DesignPanel, Panel, Template} from 'myprint-design';
+import {DesignPanel} from 'myprint-design';
+import {template} from "../constant/constant.ts";
 
 const emit = defineEmits(['back'])
-
-const templateContent = {
-  width: 100,
-  height: 100,
-  pageUnit: 'mm',
-  elementList: [{
-    type: 'Text',
-    x: 10,
-    y: 10,
-    width: 50,
-    height: 10,
-    data: '磨刀霍霍向厨房',
-    option: {
-      textAlign: 'center',
-      verticalAlign: 'center'
-    }
-  },
-    {// 时间
-      type: 'TextTime',
-      x: 10,
-      y: 20,
-      width: 50,
-      height: 10,
-      option: {
-        formatter: '{{yyyy年MM月dd日 hh:mm:ss}}',
-        textAlign: 'center',
-        verticalAlign: 'center'
-      }
-    },
-    {// 图片
-      type: 'Image',
-      x: 10,
-      y: 30,
-      width: 20,
-      height: 20
-    }, {// 横实线
-      type: 'Image',
-      x: 10,
-      y: 30,
-      width: 20,
-      height: 20
-    }]
-} as Panel;
-const template = {
-  content: JSON.stringify(templateContent)
-} as Template;
 
 </script>
 
@@ -60,7 +15,7 @@ const template = {
                 :module="{
     provider: JSON.stringify({width: 100, height: 100, pageUnit: 'mm'}),
     previewData: '[{}]'
-  }" />
+  }"/>
 </template>
 
 <style>
